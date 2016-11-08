ba6c2c254170dca7b2f6b00ffeaf758ab9dbbf82
Victor Delannoy <victor.delannoy@gmail.com>
2016-11-08 18:02:24 +0100

    ACE editor

---
+++ app/assets/javascripts/articles/form.js
@@ -0,0 +1,14 @@
+function startEditor() {
+  var editor = ace.edit("editor");
+  editor.setTheme("ace/theme/tomorrow");
+  editor.getSession().setMode("ace/mode/markdown");
+  editor.setValue($('form').find("input[id=article_content]")[0].value);
+
+}
+startEditor();
+
+$("form").on("submit", function(event) {
+  // Code executed when a form is submitted
+  var editor = ace.edit("editor");
+  $(this).find("input[id=article_content]")[0].value = editor.getValue();
+})
