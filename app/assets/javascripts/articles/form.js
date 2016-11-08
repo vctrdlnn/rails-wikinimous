cc37689db93dd0e4085b355405b693198eb1f89d
Victor Delannoy <victor.delannoy@gmail.com>
2016-11-08 21:52:40 +0100

    model sessions

--- app/assets/javascripts/articles/form.js
+++ app/assets/javascripts/articles/form.js
@@ -1,14 +1,14 @@
-function startEditor() {
-  var editor = ace.edit("editor");
-  editor.setTheme("ace/theme/tomorrow");
-  editor.getSession().setMode("ace/mode/markdown");
-  editor.setValue($('form').find("input[id=article_content]")[0].value);
+// function startEditor() {
+//   var editor = ace.edit("editor");
+//   editor.setTheme("ace/theme/tomorrow");
+//   editor.getSession().setMode("ace/mode/markdown");
+//   editor.setValue($('#article_content').val());
+// }
+// startEditor();
 
-}
-startEditor();
-
-$("form").on("submit", function(event) {
-  // Code executed when a form is submitted
-  var editor = ace.edit("editor");
-  $(this).find("input[id=article_content]")[0].value = editor.getValue();
-})
+// $("form").on("submit", function(event) {
+//   // Code executed when a form is submitted
+//   var editor = ace.edit("editor");
+//   $(this).find("input[id=article_content]")[0].value = editor.getValue();
+// })
+//
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  #routes to sessions
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
+
+  #routes to users
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles
 end
